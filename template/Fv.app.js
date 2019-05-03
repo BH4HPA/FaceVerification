a36925695680c02269569008e965e0695824720c
“LF112” <lf@lf112.net>
2019-05-03 15:08:53 +0800

    构建JavaScript

---
+++ template/Fv.app.js
@@ -0,0 +1,17 @@
+// Javascript faceVerification - Tencent cloud face recognition experience.
+// Author @LF112 (https://github.com/LF112)
+// Copyright reservation is required.
+
+let Fv = function(Fv) {
+    return new Promise(function() {
+        console.clear();
+
+        if ($(Fv.i).length > 0) {
+            //导入Fv Main
+
+
+
+        } else console.log('Fv -> 元素不存在')
+
+    })
+}
